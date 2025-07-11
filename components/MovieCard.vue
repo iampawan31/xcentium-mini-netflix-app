99b42d00683138a283b6d2d091daf558f4f2b4d2
Pawan Kumar <surpawan@gmail.com>
2025-07-11 22:07:04 +0530

    chore: code-cleanup

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -12,13 +12,13 @@
 <template>
   <NuxtLink
     :to="movieLink"
-    class="movie-card-wrapper"
+    class="block bg-white rounded-lg shadow hover:shadow-xl transition overflow-hidden p-2 transition-all"
   >
     <article v-motion-pop-bottom>
       <img
         :src="movie.Poster"
         :alt="movie.Title"
-        class="movie-card-image"
+        class="w-full h-full md:h-96 relative object-cover rounded-lg overflow-hidden hover:shadow-xl hover:scale-110 transition-all"
       />
     </article>
   </NuxtLink>
